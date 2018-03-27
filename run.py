ce5e8833a8c25bb43a98835426de90c28815fa90
Sam Escolas <samescolas@gmail.com>
2018-03-27 09:49:34 -0400

    Updating scraper to only focux on UK.

--- run.py
+++ run.py
@@ -15,33 +15,27 @@ except:
 		'data_dir': './data'
 	}
 
-def perform_extraction(countries):
-	for country in countries:
+config['base_url'] = 'https://www.bike-parts-honda.com'
+
+def perform_extraction(pages):
+	for page in pages:
 		p = Proxy()
 		# Get a few random proxies (don't want to overload
 		# proxy API (230 max requests) or send too many
 		# from the same IP continuously
 		proxies = [ p.get_proxy() for i in range(3) ]
-		db = Database(config['data_dir'], country.replace(' ', '_'))
-		s = BikeScraper(proxies, country, config['base_url'], config['image_dir'], True, True)
+		db = Database(config['data_dir'], 'united_kingdom')
+		s = BikeScraper(proxies, 'united kingdom', config['base_url'], config['image_dir'], True, False)
 		# start on page 1
-		page = 1
 		bikes = s.get_bikes(page)
-		while bikes:
-			db.save(bikes)
-			page += 1
-			time.sleep(random.randint(1, 15))
-			bikes = s.get_bikes(page)
+		db.save(bikes)
+		time.sleep(random.randint(1, 15))
 
-
-# Generate list of countries from pulled list
-with open('./countries.txt') as fd:
-	countries = [c.strip() for c in fd.readlines()]
 
 cpus = multi.cpu_count()
 workers = []
 
-page_bins = np.array_split(countries, cpus)
+page_bins = np.array_split([i for i in range(1, 144)], cpus)
 
 for cpu in range(cpus):
 	print("Starting scraper on CPU {}.".format(cpu))
