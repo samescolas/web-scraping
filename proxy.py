8b485f957b01c1feba52a1a993c8a67ae7041c24
Sam Escolas <samescolas@gmail.com>
2018-03-27 07:41:32 -0400

    Adding second source for proxy IPs.

--- proxy.py
+++ proxy.py
@@ -6,15 +6,32 @@ class Proxy:
 		self.ip = ''
 		self.port = ''
 		self.api_requests = 0
+		self.resources = [
+			{
+				'API': 'http://pubproxy.com/api/proxy',
+				'limit': 100
+			},
+			{
+				'API': 'https://gimmeproxy.com/api/getProxy',
+				'limit': 230
+			}
+		]
+		self.resource_ix = 0
 
 	def get_proxy(self):
 		# gimmeproxy rate limits @ 230 requests
-		if self.api_requests > 200:
-			return False
+		if self.api_requests > self.resources[self.resource_ix]['limit']:
+			if self.resource_ix < len(self.resources):
+				self.resource_ix += 1
+				self.api_requests = 0
+			else:
+				return False
 		try:
 			self.api_requests += 1
-			response = requests.get('https://gimmeproxy.com/api/getProxy')
+			response = requests.get(self.resources[self.resource_ix]['API'])
 			data = response.json()
+			if self.resource_ix == 0:
+				data = data['data'][0]
 			self.ip = data['ip']
 			self.port = data['port']
 		except:
